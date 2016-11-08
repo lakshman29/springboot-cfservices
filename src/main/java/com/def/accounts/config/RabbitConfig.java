f0077a8b6bc2aea5b8c059d8a7eed48f1c9765eb
lakshman29 <lakshman29@gmail.com>
2016-11-08 16:40:09 +0530

    added the datasource

--- src/main/java/com/def/accounts/config/RabbitConfig.java
+++ src/main/java/com/def/accounts/config/RabbitConfig.java
@@ -1,13 +1,15 @@
 package com.def.accounts.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
 
 @Configuration
 @ComponentScan
@@ -28,4 +30,18 @@ public class RabbitConfig extends AbstractCloudConfig {
 		//return new Queue("test"); 
 		return admin;
 }
+	
+	@Bean public DataSource dataSource() {  
+		return connectionFactory().dataSource(); 
+		}
+	
+	/*@Bean
+    SendGrid sendGrid(ObjectMapper objectMapper) throws Exception {
+        JsonNode credentials = objectMapper
+                .readValue(System.getenv("VCAP_SERVICES"), JsonNode.class).get("sendgrid")
+                .get(0).get("credentials");
+        String username = credentials.get("username").asText();
+        String password = credentials.get("password").asText();
+        return new SendGrid(username, password);
+    }*/
 }
