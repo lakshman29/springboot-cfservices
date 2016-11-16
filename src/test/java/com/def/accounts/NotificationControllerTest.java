3e3afc1b751df9552b735d2382f62aa9b6fc1e54
Lakshman <lakshman29@gmail.com>
2016-11-16 13:44:32 +0530

    modified the notification test

--- src/test/java/com/def/accounts/NotificationControllerTest.java
+++ src/test/java/com/def/accounts/NotificationControllerTest.java
@@ -3,11 +3,29 @@ package com.def.accounts;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.def.accounts.controllers.AuthenticationController;
+import com.sendgrid.SendGrid;
 
 public class NotificationControllerTest {
 	MockMvc mockMvc;
+	@InjectMocks
+	AuthenticationController controller;
+	@Mock
+	SendGrid sendGrid;
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+
+	    this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
 	@Test
 	public void testSendMail() throws Exception {
 		mockMvc.perform(post("/sendMail").param("from", "lakshman29@gmail.com").
